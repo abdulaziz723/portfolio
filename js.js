069b33d092c48dc7d940ac7553fb9f7214b6eb62
abdulaziz723 <oqilovabdulaziz7@gmai.com>
2022-02-06 22:15:37 +0500

    start

---
+++ js.js
@@ -0,0 +1,10 @@
+var type = new Typed('.text',{
+  strings:[ 'start','Vue.js','JavaScript(ES6)','Gulp','Bootstrap5','Css3','Html5','Git','Github',';-)'],
+  typeSpeed:40,
+  smartBackspace:true,
+  backSpeed: 25,
+  loop:true,
+  startDelay: 2000,
+  backDelay:1000,
+});
+// this libary html(57)
